d6e10509bd9e862fd37d6b5797196363152e9a52
jiajia.ji <jiajia.ji@huifu.com>
2022-08-01 15:25:12 +0800

    购物车业务开发

--- src/main/java/com/huifu/reggie/common/MyMetaObjectHandler.java
+++ src/main/java/com/huifu/reggie/common/MyMetaObjectHandler.java
@@ -24,7 +24,6 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         metaObject.setValue("updateTime", LocalDateTime.now());
         metaObject.setValue("createUser", BaseContext.getCurrentId());
         metaObject.setValue("updateUser", BaseContext.getCurrentId());
-
     }
 
     /**
--- src/main/java/com/huifu/reggie/controller/ShoppingCartController.java
+++ src/main/java/com/huifu/reggie/controller/ShoppingCartController.java
@@ -1,16 +1,87 @@
 package com.huifu.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.huifu.reggie.common.BaseContext;
+import com.huifu.reggie.common.R;
+import com.huifu.reggie.entity.Dish;
+import com.huifu.reggie.entity.Setmeal;
+import com.huifu.reggie.entity.ShoppingCart;
 import com.huifu.reggie.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 @RestController
-@RequestMapping
+@RequestMapping("/shoppingCart")
 @Slf4j
 public class ShoppingCartController {
 
     @Autowired
     private ShoppingCartService shoppingCartService;
+
+    /**
+     * 添加购物车
+     *
+     * @param shoppingCart
+     * @return
+     */
+    @PostMapping("/add")
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart) {
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+
+        Long dishId = shoppingCart.getDishId();
+        Long setmealId = shoppingCart.getSetmealId();
+        LambdaQueryWrapper<ShoppingCart> shoppingCartLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        shoppingCartLambdaQueryWrapper.eq(ShoppingCart::getUserId, shoppingCart.getUserId());
+
+        if (null != dishId) {
+            //添加到购物车到是菜品
+            shoppingCartLambdaQueryWrapper.eq(ShoppingCart::getDishId, dishId);
+
+        } else if (null != setmealId) {
+            //添加到购物车到是套餐
+            shoppingCartLambdaQueryWrapper.eq(ShoppingCart::getSetmealId, setmealId);
+        }
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(shoppingCartLambdaQueryWrapper);
+        //判断库里是否存在，存在则在原来基础上+1
+        if (cartServiceOne != null) {
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number + 1);
+            shoppingCartService.updateById(cartServiceOne);
+        } else {
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            cartServiceOne = shoppingCart;
+            shoppingCartService.save(cartServiceOne);
+
+        }
+        return R.success(cartServiceOne);
+    }
+
+
+    /**
+     * 查看购物车
+     *
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<ShoppingCart>> list() {
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+        queryWrapper.orderByDesc(ShoppingCart::getCreateTime);
+        List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
+        return R.success(list);
+    }
+
+    @DeleteMapping("/clean")
+    public R<String> clean() {
+        LambdaQueryWrapper<ShoppingCart> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+        shoppingCartService.remove(lambdaQueryWrapper);
+        return R.success("清空成功");
+    }
 }
